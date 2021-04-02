e97b800981f08151d749ef6495e81e34f946e496
shihabmilky1 <shihabmilky1@gmail.com>
2021-04-02 20:12:39 +0600

    canchel order added

--- src/components/Orders/UserOrder.js
+++ src/components/Orders/UserOrder.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useHistory } from 'react-router';
 
 const UserOrder = (props) => {
-  console.log(props);
     const {_id} = props.orders;
     const {imageURL,name,quantity,price} = props.orders.orders;
     const history = useHistory();
